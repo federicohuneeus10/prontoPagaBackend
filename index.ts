ad94e2605ddc30374af41cc0394e19275a92443f
Federico Huneeus <federicohuneeus10@gmail.com>
2026-09-08 19:25:29 -0300

    adding cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import authRouter from "./src/routes/auth.routes";
 import scoreRouter from "./src/routes/score.routes";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5176" }));
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/score", scoreRouter);
